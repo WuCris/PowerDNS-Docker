0d7bf633216b3adf376c0594569324e7ef3a15d9
user <user@localhost.localdomain>
2023-12-20 12:02:41 -0500

    improve env replacement:

--- powerdns/docker-entrypoint.sh
+++ powerdns/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
-envsubst < /etc/pdns/pdns.d/mariadb.conf >> /etc/pdns/pdns.d/mariadb.conf
+envsubst < /etc/default/template/pdns.d/mariadb.conf > /etc/pdns/pdns.d/mariadb.conf
+envsubst < /etc/default/template/pdns.d/api.conf > /etc/pdns/pdns.d/api.conf
 
 exec "$@"
